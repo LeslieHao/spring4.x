09794da09a2ebab8853b138414d905d9e7720737
haohao <imLeslieHao@163.com>
2018-12-12 23:11:33 +0800

    // bean

--- src/main/java/com/hh/spring4/x/section5/TestBeanFactory.java
+++ src/main/java/com/hh/spring4/x/section5/TestBeanFactory.java
@@ -1,6 +1,7 @@
 package com.hh.spring4.x.section5;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.BeanFactoryAware;
@@ -8,18 +9,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * DESC: BeanFactory 面向Spring
  * ApplicationContext 面Spring 使用者者
  * Created by Hh on 2018/1/16
  */
-@DirtiesContext //待整个测试类的所有测试执行结束后，该测试的application context会被关闭，同时缓存会清除
+//@DirtiesContext //待整个测试类的所有测试执行结束后，该测试的application context会被关闭，同时缓存会清除
+@RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:spring-context.xml"})
-public class TestBeanFactory extends AbstractTransactionalJUnit4SpringContextTests implements BeanFactoryAware {
+public class TestBeanFactory implements BeanFactoryAware {
 
     @Autowired
-    private UserService userService;
+    private TestService testService;
 
     private BeanFactory beanFactory;
 
---
+++ src/main/java/com/hh/spring4/x/section5/TestService.java
@@ -0,0 +1,11 @@
+package com.hh.spring4.x.section5;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author HaoHao
+ * @date 2018/12/12下午11:08
+ */
+@Service
+public class TestService {
+}
